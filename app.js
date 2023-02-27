0577789251d269078fce1f1005b1907b27a26d61
Sobhangh <sobhan.gharanfoli@gmail.com>
2023-02-27 16:55:13 +0100

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { app, query, errorHandler } from 'mu';
 
 app.get('/datasets/:id/subsets', function(req, res) {
-  res.send('Hello mu-javascript-template');
+  res.send('Hello mu-javascript-template.....');
 } );
 
 app.use(errorHandler);
